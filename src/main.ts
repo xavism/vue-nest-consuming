b613138d12fb8cfdd4b64b018f735ad014c8a3c2
Xavi Sánchez Mir <xavi.sanchez.mir@gmail.com>
2018-02-22 15:23:22 +0100

    Updating linter problems

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router.ts';
-import store from './store.ts';
-import './registerServiceWorker.ts';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
